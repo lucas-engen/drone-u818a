ceea0dd0e4f6e2cf1bfebe3853650713d0894215
Lucas Vieira <lucas.engen.cc@gmail.com>
2018-06-08 23:52:11 -0300

    - A árvore de pastas do projeto foi reorganizada - Criado script para automatizar a parte de unir todos os arquivos e realizar o assembly - Adicionado um passo a passo de como contribuir com o projeto - Adicionado um passo a passo de como instalar o Git - O manual do drone e o pré projeto foram colocados na pasta documentos

---
+++ assembly.bat
@@ -0,0 +1,34 @@
+:: Este script faz parte do projeto final de diac - Drone U818A
+::
+:: Função: automatizar a tarefa de unir todos os arquivos CATPart e CATDrawing em uma unica pasta
+:: para poder criar o assembly.
+:: 
+:: Autor: Lucas Vieira
+
+@echo off
+cls
+set nome_pasta_assembly="Assembly"
+if not exist %nome_pasta_assembly% (
+	echo A pasta contendo o assembly ainda não foi criada.
+	mkdir %nome_pasta_assembly%
+)
+copy /y samara\*.CATPart %nome_pasta_assembly%\>nul
+copy /y lucas\*.CATPart %nome_pasta_assembly%\>nul
+copy /y jose\*.CATPart %nome_pasta_assembly%\>nul
+copy /y pedro\*.CATPart %nome_pasta_assembly%\>nul
+copy /y samara\*.CATDrawing %nome_pasta_assembly%\>nul
+copy /y lucas\*.CATDrawing %nome_pasta_assembly%\>nul
+copy /y jose\*.CATDrawing %nome_pasta_assembly%\>nul
+copy /y pedro\*.CATPart %nome_pasta_assembly%\>nul
+
+echo.
+if %ERRORLEVEL% equ 0 (
+echo Tarefas finalizadas com sucesso.
+color a
+) else (
+echo Tarefas finalizadas com erro.
+color c
+)
+echo.
+pause
+cls && color
